e638e70b4da4b1b12fc88916d29cc57fce64fafc
unknown <codeleap@outlook.com>
2019-03-25 20:53:21 +0100

    View display most recent post html output modified.

--- admin_pages/last-post.php
+++ admin_pages/last-post.php
@@ -1,4 +1,5 @@
-<h2>Last post selected!</h2>
-   <?php for($i=0; $i<5; $i++): ?>
-      <p><?php echo $i; ?></p>
-   <?php endfor; ?>
+<h2>Here is your most recent post:</h2>
+   <h3><b>POST TITLE: </b><?php echo $results[0]->post_title; ?></h3>
+   <p><b>POST ID: </b><?php echo $results[0]->ID; ?></p>
+   <p><b>POST TYPE: </b><?php echo $results[0]->post_type; ?></p>
+   <p><b>DATE: </b><?php echo $results[0]->post_date; ?></p>
