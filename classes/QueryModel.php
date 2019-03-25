649228192b6fdbea6536d7c164a9dd3107f95595
unknown <codeleap@outlook.com>
2019-03-25 20:55:11 +0100

    Set results property to the value of the query result in the function perform_query

--- classes/QueryModel.php
+++ classes/QueryModel.php
@@ -6,5 +6,14 @@ class QueryModel{
    public function __construct($args){
       $this->args = $args;
    }
+
+   public function perform_query(){
+      $query = new WP_Query($this->args);
+      $this->results = $query;
+   }
+
+   public function return_results(){
+      return $this->results;
+   }
    
 }
