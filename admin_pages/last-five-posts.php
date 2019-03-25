bebc063c155f4609b8eabd815e283901fe32a8bd
unknown <codeleap@outlook.com>
2019-03-25 20:52:07 +0100

    View last five posts modified to display 5 most recent posts.

--- admin_pages/last-five-posts.php
+++ admin_pages/last-five-posts.php
@@ -1,8 +1,8 @@
-<h2>Last 5 posts</h2>
+<h2>Here your 5 most recent posts:</h2>
    <?php for($i=0; $i<5; $i++): ?>
-      <h3><b>POST TITLE: </b><?php echo $last_five_posts[$i]->post_title; ?></h3>
-      <p><b>POST ID: </b><?php echo $last_five_posts[$i]->ID; ?></p>
-      <p><b>POST TYPE: </b><?php echo $last_five_posts[$i]->post_type; ?></p>
-      <p><b>DATE: </b><?php echo $last_five_posts[$i]->post_date; ?></p>
+      <h3><b>POST TITLE: </b><?php echo $results[$i]->post_title; ?></h3>
+      <p><b>POST ID: </b><?php echo $results[$i]->ID; ?></p>
+      <p><b>POST TYPE: </b><?php echo $results[$i]->post_type; ?></p>
+      <p><b>DATE: </b><?php echo $results[$i]->post_date; ?></p>
       <p>---------------------------------</p>
    <?php endfor; ?>
