9d6ed324023b78290eed4e671a81cf76446862a2
unknown <codeleap@outlook.com>
2019-03-26 19:32:02 +0100

    Last post view page modified font size.

--- admin_pages/last-post.php
+++ admin_pages/last-post.php
@@ -1,5 +1,5 @@
-<h2>Here is your most recent post:</h2>
-   <h3><b>POST TITLE: </b><?php echo $results[0]->post_title; ?></h3>
-   <p><b>POST ID: </b><?php echo $results[0]->ID; ?></p>
-   <p><b>POST TYPE: </b><?php echo $results[0]->post_type; ?></p>
-   <p><b>DATE: </b><?php echo $results[0]->post_date; ?></p>
+<h2>This is your most recent post:</h2>
+   <h4>Post Title: <?php echo $results[0]->post_title; ?></h4>
+   <h4>Post ID: <?php echo $results[0]->ID; ?></h4>
+   <h4>Post Type: <?php echo $results[0]->post_type; ?></h4>
+   <h4>Date: <?php echo $results[0]->post_date; ?></h4>
