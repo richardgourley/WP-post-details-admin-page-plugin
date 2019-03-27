43dcdd34d9175df824c935bd5f6afe3278d591ca
unknown <codeleap@outlook.com>
2019-03-27 20:14:27 +0100

    Added form button to main view for how long since a post was published in a post type.

--- admin_pages/main.php
+++ admin_pages/main.php
@@ -3,6 +3,15 @@
 <h1 style='padding:15px; border: 2px solid grey; margin-right:13px;'>Hello there <span style='border-radius:3px;padding:9px;color:white; background-color: lightblue'><?php echo $current_user->user_nicename; ?></span></h1>
 <h1 style='text-align: center'>Welcome to WP Post Details</h1>
 
+<div style="display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;">
+<div style="display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;">
+<h2>See your recent posting activity</h2>
 <form action="<?php echo esc_url( admin_url('admin.php?page=wp-post-details')); ?>" method="post">
    <input type="hidden" name="action" value="last_five_posts">
    <input type="hidden" name="last_five_posts" value="1">
@@ -14,7 +23,13 @@
    <input type="hidden" name="last_post" value="1">
    <input type="submit" name="submit" id="submit" value="Get last post">
 </form>
+</div>
 
+<div style="display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;">
+<h2>See how many posts you have</h2>
 <form action="<?php echo esc_url( admin_url('admin.php?page=wp-post-details')); ?>" method="post">
    <input type="hidden" name="action" value="num_posts_category">
    <input type="hidden" name="num_posts_category" value="1">
@@ -26,9 +41,18 @@
    <input type="hidden" name="num_posts_post_type" value="1">
    <input type="submit" name="submit" id="submit" value="Get number of posts by post type">
 </form>
+</div>
 
+<div style="display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;">
+<h2>See how long it's been since you posted somewhere</h2>
 <form action="<?php echo esc_url( admin_url('admin.php?page=wp-post-details')); ?>" method="post">
-   <input type="hidden" name="action" value="since_last_post_category">
-   <input type="hidden" name="since_last_post_category" value="1">
-   <input type="submit" name="submit" id="submit" value="How long since you wrote a post for a category">
+   <input type="hidden" name="action" value="since_last_post_type">
+   <input type="hidden" name="since_last_post_type" value="1">
+   <input type="submit" name="submit" id="submit" value="Days since you last published for a post type">
 </form>
+</div>
+</div>
+
