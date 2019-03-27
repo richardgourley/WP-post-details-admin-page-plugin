e61140117507cc0624f320e7357a2642f65056db
unknown <codeleap@outlook.com>
2019-03-27 20:18:45 +0100

    Test removed from view displaying how many days since a post was published in a specific category.

--- admin_pages/since-last-post-category.php
+++ admin_pages/since-last-post-category.php
@@ -1,7 +1,8 @@
 <h2>This is how long since you have written a post for a specific category:</h2>
 
+<?php var_dump($results); ?>
 <?php foreach($results as $arr): ?>   
-   <h4>Category Name:  <?php echo $arr->post_type; ?></h4>
-   <h4>Days since post published:  <?php echo $arr->COUNT; ?></h4>
+   <h4>Category Name:  <?php echo $arr->category_name; ?></h4>
+   <h4>Days since post published:  <?php echo $arr->DAYS; ?></h4>
    <p>-----------------------------------</p>
 <?php endforeach; ?>
